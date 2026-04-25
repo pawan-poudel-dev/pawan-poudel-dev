b92651e147096e38f52c83c450b3b84f77e0e8d9
pawan_poudel_Devloper <poudelpawan770@gmail.com>
2026-04-25 16:59:28 +0545

    Updated About.tsx to use local image files instead of placeholder

---
+++ src/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>About Me</h2>
+            <img src="/assets/images/profile.jpg" alt="Profile" style={{ width: '100%', height: 'auto' }} />
+            <p>Hello! I'm Pawan Poudel, a developer with a passion for building web applications.</p>
+            {/* Add more about me content here */}
+        </div>
+    );
+};
+
+export default About;
